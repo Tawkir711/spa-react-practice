1e6b44fd601b40746f67cf8ca34b659bbe320e3b
Tawkir711 <mdtawkirk711@gmail.com>
2023-09-20 12:26:04 +0600

    added useNavigation and useLocation

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useNavigation } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../footer/Footer';
 
 const Home = () => {
+
+  const navigation = useNavigation();
+
+  const location = useLocation();
+  console.log(location);
+
   return (
     <div>
       <Header></Header>
+      {
+        navigation.state === 'loading' ? <p>
+          Loading............
+        </p>:
       <Outlet></Outlet>
+      }
       <Footer></Footer>
     </div>
   );
